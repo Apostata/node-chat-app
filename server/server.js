43382ead6c0df1f04d183e0ea47ddf4be87cb5bb
Rene <rene.souza@virtualideas.com.br>
2016-12-02 18:39:14 -0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,31 @@ app.use(express.static(publicPath)); //set a static public path
 io.on('connection', (socket)=>{
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from:"Admin",
+		text:"Welcome to the chat app",
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage',{
+		from:"Admin",
+		text:"A New user joined the chat",
+		createdAt: new Date().getTime()
+	})
+
 	socket.on('createMessage', function(msg){
 		console.log('Message from client', msg);
 		io.emit('newMessage', {
 			from: msg.from,
 			text: msg.text,
 			createdAt: new Date().getTime()
-		})
+		});
+
+		/*socket.broadcast.emit('newMessage',{ // socket.broadcast.emit - envia para todo mundo menos o próprio usuário quem enviou
+			from:msg.from,
+			text:msg.text,
+			createdAt: new Date().getTime()
+		});*/
 	});
 
 	socket.on('disconnect',()=>{
